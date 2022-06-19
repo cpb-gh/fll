844fd280af531d78b48e5c43a1f53d15ff74585a
Matt Coddington <coddington@gmail.com>
2022-06-19 16:43:51 +0000

    implement action to combine functions

---
+++ functions/test_function.py
@@ -0,0 +1,9 @@
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike.control import wait_for_seconds, wait_until, Timer
+from math import *
+
+### FUNCTION START
+# this is a test function, don't actually use this one
+def test_function():
+    print("test function")
+### FUNCTION END
