82ae43ee7a91e5458a142f8bc357a212efdffe85
proritobot <proritos@googlegroups.com>
2022-09-15 23:06:34 +0000

    new functions merged

--- combined_functions.py
+++ combined_functions.py
@@ -250,6 +250,28 @@ def BounceEaseInOut(t):
 ###
 
 ###
+### BEGIN FUNCTION FROM FILE: functions/utillity_functions.py
+###
+
+def get_port_by_letter(port):
+    if port ==  'A':
+        return hub.port.A.motor
+    if port == 'B':
+        return hub.port.B.motor
+    if port == 'C':
+        return hub.port.C.motor
+    if port == 'D':
+        return hub.port.D.motor
+    if port == 'E':
+        return hub.port.E.motor
+    if port == 'F':
+        return hub.port.F.motor
+
+###
+### END FUNCTION FROM FILE: functions/utillity_functions.py
+###
+
+###
 ### BEGIN FUNCTION FROM FILE: functions/motor_rotation_functions
 ###
 
