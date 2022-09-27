66e3a5785641ff7ff0eb5c8b158093b0e13d2c9d
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-09-27 19:38:04 -0400

    Update gyro_straight.py
    added sensed black

--- in_progress/gyro_straight.py
+++ in_progress/gyro_straight.py
@@ -1,20 +1,17 @@
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
-import hub  
+import hub
 
-color_sensor_one = ColorSensor('C')
-color_sensor_two = ColorSensor('D')
- 
-def sesnsed_black():
-   color_one = color_sensor_one.get_color()
-   color_two = color_sensor_two.get_color()
-   if color_one = 'black' and color_two = 'black':
-       return True
-   else:
-       return False
-
-
+def sensed_black(letter_one = 'C', letter_two = 'D'):
+    color_sensor_one = ColorSensor(letter_one)
+    color_sensor_two = ColorSensor(letter_two)
+    color_one = color_sensor_one.get_color()
+    color_two = color_sensor_two.get_color()
+    if color_one == 'black' and color_two == 'black':
+        return True
+    else:
+        return False
 
 def get_motor_by_letter(port):
     if port =='A':
@@ -32,15 +29,17 @@ def get_motor_by_letter(port):
 
 def gyro_straight( left_motor_letter='B', right_motor_letter='A', degrees=9000, start_power=100, end_power=50, easing='LINEAR', motor_stop_mode='BRAKE', also_stop_if = None ):
     motor_pair = MotorPair(left_motor_letter, right_motor_letter)
-    motor_pair.start_tank(start_power, start_power)
     motor_left = get_motor_by_letter(left_motor_letter)
     motor_right = get_motor_by_letter(right_motor_letter)
-    speed, relative_degrees, absolute_degrees, pwm = motor_left.get()
+    # motor_right.preset(0) will reset the relative degrees because otherwise the second time you run this function the relative degrees will start where it left off last time 
+    motor_right.preset(0)
+    speed, relative_degrees, absolute_degrees, pwm = motor_right.get()
+    print("Start:", speed, relative_degrees, absolute_degrees, pwm)
+    motor_pair.start_tank(start_power, start_power)
     while True:
         speed, relative_degrees, absolute_degrees, pwm = motor_right.get()
-        print(relative_degrees)
-        if relative_degrees >= degrees:
+        #print(speed, relative_degrees, absolute_degrees, pwm)
+        if also_stop_if() == True or relative_degrees >= degrees:
             motor_pair.stop()
             return
-        wait_for_seconds(.1)
-gyro_straight(start_power= 50000 ,degrees = 5000)
+        #wait_for_seconds(.5)
