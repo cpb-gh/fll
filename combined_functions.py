4e6b2ec3b2648c7eaae4fc5e5293b8c87b1ec642
proritobot <proritos@googlegroups.com>
2022-10-02 20:03:39 +0000

    new functions merged

--- combined_functions.py
+++ combined_functions.py
@@ -9,18 +9,6 @@ import hub
 
 
 ###
-### BEGIN FUNCTION FROM FILE: functions/test_function.py
-###
-
-# this is a test function, don't actually use this one
-def test_function():
-    print("test function")
-
-###
-### END FUNCTION FROM FILE: functions/test_function.py
-###
-
-###
 ### BEGIN FUNCTION FROM FILE: functions/easing_functions.py
 ###
 
@@ -251,6 +239,83 @@ def BounceEaseInOut(t):
 ###
 
 ###
+### BEGIN FUNCTION FROM FILE: functions/test_function.py
+###
+
+# this is a test function, don't actually use this one
+def test_function():
+    print("test function")
+
+###
+### END FUNCTION FROM FILE: functions/test_function.py
+###
+
+###
+### BEGIN FUNCTION FROM FILE: functions/gyro_straight.py
+###
+
+def sensed_black(letter_one = 'C', letter_two = 'D'):
+    color_sensor_one = ColorSensor(letter_one)
+    color_sensor_two = ColorSensor(letter_two)
+    color_one = color_sensor_one.get_color()
+    color_two = color_sensor_two.get_color()
+    if color_one == 'black' and color_two == 'black':
+        return True
+    else:
+        return False
+
+def gyro_straight( left_motor_letter='B', right_motor_letter='A', degrees=9000, start_power=100, end_power=50, easing='LINEAR', motor_stop_mode='BRAKE', also_stop_if = None ):
+    motor_pair = MotorPair(left_motor_letter, right_motor_letter)
+    motor_left = get_motor_by_letter(left_motor_letter)
+    motor_right = get_motor_by_letter(right_motor_letter)
+    # motor_right.preset(0) will reset the relative degrees because otherwise the second time you run this function the relative degrees will start where it left off last time 
+    motor_right.preset(0)
+    speed, relative_degrees, absolute_degrees, pwm = motor_right.get()
+    print("Start:", speed, relative_degrees, absolute_degrees, pwm)
+    motor_pair.start_tank(start_power, start_power)
+    while True:
+        speed, relative_degrees, absolute_degrees, pwm = motor_right.get()
+        if also_stop_if() == True or relative_degrees >= degrees:
+            if motor_stop_mode == 'BRAKE':
+                motor_right.brake()
+                motor_left.brake()
+            elif motor_stop_mode == 'HOLD':
+                motor_right.hold()
+                motor_left.hold()
+            elif motor_stop_mode == 'FLOAT':
+                motor_right.float()
+                motor_left.float()
+            else:
+                print("check your spelling of your motor_stop_mode:", motor_stop_mode )
+            return
+
+###
+### END FUNCTION FROM FILE: functions/gyro_straight.py
+###
+
+###
+### BEGIN FUNCTION FROM FILE: functions/utillity_functions.py
+###
+
+def get_motor_by_letter(port):
+    if port ==  'A':
+        return hub.port.A.motor
+    if port == 'B':
+        return hub.port.B.motor
+    if port == 'C':
+        return hub.port.C.motor
+    if port == 'D':
+        return hub.port.D.motor
+    if port == 'E':
+        return hub.port.E.motor
+    if port == 'F':
+        return hub.port.F.motor
+
+###
+### END FUNCTION FROM FILE: functions/utillity_functions.py
+###
+
+###
 ### BEGIN FUNCTION FROM FILE: functions/motor_rotation_functions.py
 ###
 
@@ -325,26 +390,4 @@ def party_mode(color_sensor_one = 'C', color_sensor_two = 'D', party_length = 20
 
 ###
 ### END FUNCTION FROM FILE: functions/party_mode.py
-###
-
-###
-### BEGIN FUNCTION FROM FILE: functions/utillity_functions.py
-###
-
-def get_motor_by_letter(port):
-    if port ==  'A':
-        return hub.port.A.motor
-    if port == 'B':
-        return hub.port.B.motor
-    if port == 'C':
-        return hub.port.C.motor
-    if port == 'D':
-        return hub.port.D.motor
-    if port == 'E':
-        return hub.port.E.motor
-    if port == 'F':
-        return hub.port.F.motor
-
-###
-### END FUNCTION FROM FILE: functions/utillity_functions.py
 ###
