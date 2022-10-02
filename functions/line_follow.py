20c79384432786ce4050605170e8c2d3d4fbae72
sithforri <104786356+sithforri@users.noreply.github.com>
2022-10-02 16:00:40 -0400

    line follow and turn_code (#6)
    * new file
    
    initial commit
    
    * added func boundaries
    
    * fixed syntax
    
    * Update functions/line_follow.py
    
    Co-authored-by: cpb-gh <104538915+cpb-gh@users.noreply.github.com>
    
    * Create turn_code.py
    
    * fixed easing
    
    expects an ease function
    
    * Update line_follow.py
    
    * Update turn_code.py
    
    Co-authored-by: cpb-gh <104538915+cpb-gh@users.noreply.github.com>

---
+++ functions/line_follow.py
@@ -0,0 +1,63 @@
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike.control import wait_for_seconds, wait_until, Timer
+from math import *
+
+def get_speed( low_speed, high_speed, pct ):
+    return low_speed+((high_speed-low_speed)*pct)
+### FUNCTION START
+
+def line_follow( Sspeed=40, Espeed=20, sensorLetter="D", 
+                 stopIf=None, stopMode='brake', ease = None, degrees=1000,
+                 motorLeftletter = 'A', motorRightletter='B'):
+    
+    motor_pair = MotorPair(motorLeftletter, motorRightletter)
+    motor1 = Motor(motorLeftletter)
+    motor2 = Motor(motorRightletter)
+
+    color = ColorSensor(sensorLetter)
+    stop = False
+    integral = 0
+    lastError = 0
+    
+    motor1.set_degrees_counted(0)
+    motor2.set_degrees_counted(0)
+    if stopMode is not None:
+        motor_pair.set_stop_action(stopMode)
+    while stop == False:
+        mdeg1 = motor1.get_degrees_counted()
+        mdeg2 = motor2.get_degrees_counted()
+        motordeg = (abs(mdeg1) + abs(mdeg2))/ 2
+        if motordeg >= degrees:
+            stop = True
+
+        pct = motordeg/degrees 
+        #if ease is not None:
+        #    pct = ease( pct )
+        
+        speedy = get_speed( Sspeed, Espeed, pct )
+        # print(  'speedy', speedy )
+
+
+
+        speed = speedy
+
+        error = color.get_reflected_light() - 50
+        P_fix = error * 0.3
+
+        integral = integral + error
+        
+        I_fix = integral * 0.001
+
+        derivative = error - lastError
+        lastError = error
+        D_fix = derivative * 1
+
+        correction = P_fix+I_fix+D_fix
+
+        motor_pair.start_tank_at_power(int(speed-correction), int(speed+correction))
+
+        if stop == True and stopMode is not None:
+            motor_pair.stop()
+ 
+### FUNCTION END
+
---
+++ functions/turn_code.py
@@ -0,0 +1,60 @@
+
+from spike import PrimeHub,MotorPair
+
+### FUNCTION START
+def get_speed(start, end, percent):
+    return int ( start + (end - start)*percent )
+
+
+
+def turn_function(degrees=90, ease=None, stoptype='brake',
+    startspeed=20, endspeed=40, motorletterleft='A', motorletterright='B',
+    turntype='both'):
+
+    neg = degrees<0
+
+    hub = PrimeHub()
+
+
+    hub.motion_sensor.reset_yaw_angle()
+    motors = MotorPair(motorletterleft, motorletterright)
+    keep_spinning = True
+    while keep_spinning == True:
+        degrees_now= hub.motion_sensor.get_yaw_angle()
+        if neg and degrees_now <= degrees :
+            keep_spinning = False
+        elif not neg and degrees_now >= degrees :
+            keep_spinning = False
+
+        if keep_spinning:
+            pct = degrees_now/degrees
+  
+            if ease is not None:
+                pct = ease(pct)
+            speed = get_speed (startspeed, endspeed, pct)
+
+            if turntype is 'both':
+                if neg:
+                    motors.start_tank_at_power(-speed, speed)
+                else:
+                    motors.start_tank_at_power(speed, -speed)
+            elif turntype is 'left':
+                if neg:
+                    motors.start_tank_at_power(-speed, 0)
+                else:
+                    motors.start_tank_at_power(speed, 0)
+            elif turntype is 'right':
+                if neg:
+                    motors.start_tank_at_power(0,speed)
+                else:
+                    motors.start_tank_at_power(0,-speed)
+
+
+    if stoptype is not None:
+        motors.set_stop_action( stoptype )
+        motors.stop()
+
+### FUNCTION END
+
+turn_function (degrees=-90, ease=None, stoptype='brake',
+    startspeed=20, endspeed=40, motorletterleft='A', motorletterright='B', turntype='right')
