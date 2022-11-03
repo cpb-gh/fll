9c2ce26cd043647b4b4baba27aa5546751365f32
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-11-03 19:29:45 -0400

    Update start_run.py

--- functions/start_run.py
+++ functions/start_run.py
@@ -10,17 +10,17 @@ def start_run( color_sensor_letter = 'C', delay = 1):
     while True:
         the_color = color.get_color()
         if the_color == 'red':
-            print ( 'Detected:' the_color)
+            print ( 'Detected:', the_color)
             status_light.on('red')
             speaker.beep(60, delay)
             #call run 1 
         elif the_color == 'yellow':
-            print ( 'Detected:' the_color)
+            print ( 'Detected:', the_color)
             status_light.on('red')
             speaker.beep(60, delay)
             #call run 1
         elif the_color == 'blue':
-            print ('Detected;', the_color)
+            print ('Detected:', the_color)
             status_light.on('blue')
             speaker.beep(100, delay)
             #call run 1
