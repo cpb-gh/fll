7b22bc7318aff984a389026236f7d0aa3d84032c
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-11-03 19:58:55 -0400

    Update run_1.py

--- functions/run_1.py
+++ functions/run_1.py
@@ -3,3 +3,4 @@ def run_one():
     gyro_straight(degrees = 900, start_power = 70, end_power = 100, easing = ExponentialEaseOut, motor_stop_modde = brake)
     gyro_straight(degrees = -100, start_power = 30, end_power = 30)
     turn_function(degrees = -25, easing = None, stoptype = 'brake', startspeed = 60, endspeed = 50,  motorletterleft = 'A', motorletterright = 'B', turntype = 'both')
+### FUNCTION END
