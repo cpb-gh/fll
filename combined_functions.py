1440a791f863c8314cc81f626192560b196c4609
proritobot <proritos@googlegroups.com>
2022-11-06 20:23:04 +0000

    new functions merged

--- combined_functions.py
+++ combined_functions.py
@@ -490,25 +490,31 @@ def zz_run_one():
 ###
 
 
+
 def start_run( color_sensor_letter = 'C', delay = 1):
+    # color_sensor_letter = 'C' means that the active sensor is on the right side of the robot when it is facing the same direction as you
     color = ColorSensor(color_sensor_letter)
     status_light = StatusLight()
     speaker = Speaker()
-    while True:
+    waiting_for_run = True
+    while waiting_for_run:
         the_color = color.get_color()
         if the_color == 'red':
             print ( 'Detected:', the_color)
             status_light.on('red')
             speaker.beep(60, delay)
             zz_run_one()
+            waiting_for_run = False
         elif the_color == 'yellow':
             print ( 'Detected:', the_color)
             status_light.on('red')
             speaker.beep(60, delay)
             #zz_run_two()
+            waiting_for_run = False
         else:
             print ('not a run color:', the_color)
             status_light.off()
+
 
 
 
