bcd84c1ff4f7a3ca697fae8f779745ee4987daed
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-11-06 15:22:49 -0500

    Update start_run.py
    BUGGER FIXES

--- functions/start_run.py
+++ functions/start_run.py
@@ -3,25 +3,31 @@ from spike.control import wait_for_seconds
 
 ### FUNCTION START
 
+
 def start_run( color_sensor_letter = 'C', delay = 1):
+    # color_sensor_letter = 'C' means that the active sensor is on the right side of the robot when it is facing the same direction as you
     color = ColorSensor(color_sensor_letter)
     status_light = StatusLight()
     speaker = Speaker()
-    while True:
+    waiting_for_run = True
+    while waiting_for_run:
         the_color = color.get_color()
         if the_color == 'red':
             print ( 'Detected:', the_color)
             status_light.on('red')
             speaker.beep(60, delay)
             zz_run_one()
+            waiting_for_run = False
         elif the_color == 'yellow':
             print ( 'Detected:', the_color)
             status_light.on('red')
             speaker.beep(60, delay)
             #zz_run_two()
+            waiting_for_run = False
         else:
             print ('not a run color:', the_color)
             status_light.off()
+
 
 ### FUNCTION END
 start_run(color_sensor_letter = 'D', delay = 5)
