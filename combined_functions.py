f53ba0857fd71ded32296e0e39002c2be8b94ef9
proritobot <proritos@googlegroups.com>
2022-11-06 20:17:15 +0000

    new functions merged

--- combined_functions.py
+++ combined_functions.py
@@ -480,7 +480,7 @@ def party_mode(color_sensor_one = 'C', color_sensor_two = 'D', party_length = 20
 ###
 
 def zz_run_one():
-    gyro_straight(degrees = 900, start_power = 70, end_power = 100, easing = ExponentialEaseOut, motor_stop_modde = brake)
+    gyro_straight(degrees = 900, start_power = 70, end_power = 100, easing = ExponentialEaseOut, motor_stop_mode = brake)
     gyro_straight(degrees = -100, start_power = 30, end_power = 30)
     turn_function(degrees = -25, easing = None, stoptype = 'brake', startspeed = 60, endspeed = 50,  motorletterleft = 'A', motorletterright = 'B', turntype = 'both')
 
