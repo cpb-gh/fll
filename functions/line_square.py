68d15d33e6f199f39776f4cda797e7029d5a26c3
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-11-10 20:03:47 -0500

    Update line_square.py
    we did some basic changes to help make this function more realistic for the way we are going to be using it.

--- functions/line_square.py
+++ functions/line_square.py
@@ -1,21 +1,39 @@
-from spike import MotorPair, ColorSensor
 
+from spike import PrimeHub, MotionSensor, Motor, MotorPair, ColorSensor
+from spike.control import wait_for_seconds, wait_until, Timer
+import math
+import hub
 from spike.control import wait_until
 
 ### FUNCTION START
 
-def line_square ( speed=40, color_to_hit='black',leftfirst=True, sensorletterleft='E', sensorletterright='D', motorletterleft='A', motorletterright='B' ):
+def line_square ( speed=40, color_to_hit='black', sensorletterleft='E', sensorletterright='D', motorletterleft='A', motorletterright='B', overshoot_seconds = 0 ):
     motors = MotorPair(motorletterleft, motorletterright)
     motors.set_stop_action('brake')
     sensorL = ColorSensor ( sensorletterleft )
     sensorR = ColorSensor ( sensorletterright )
+    sensor_left_color = sensorL.get_color()
+    sensor_right_color = sensorR.get_color()
+
+    if sensor_right_color != color_to_hit and sensor_left_color != color_to_hit:
+        print("neither sensor on color_to_hit, returning...")
+        return
+
+    if sensor_right_color == color_to_hit and sensor_left_color == color_to_hit:
+        print("both sensors on color_to_hit, returning...")
+        return
+
+    if sensor_left_color == color_to_hit:
+        leftfirst = True
+    else:
+        leftfirst = False
 
     def hit_colorL():
-        print( 'left', sensorL.get_color() )
+        # print( 'left', sensorL.get_color() )
         return sensorL.get_color() == color_to_hit
 
     def hit_colorR():
-        print( 'right', sensorR.get_color() )
+        # print( 'right', sensorR.get_color() )
         return sensorR.get_color() == color_to_hit
 
     if (leftfirst == True):
@@ -34,9 +52,8 @@ def line_square ( speed=40, color_to_hit='black',leftfirst=True, sensorletterlef
             print( 'B' )
             motors.start_tank (speed, 0)
             wait_until (hit_colorL)
-
+    ### depending on what angle you hit a black line you have to overshoot with the second sensor to square it
+    wait_for_seconds(overshoot_seconds)
     motors.stop()
 
 ### FUNCTION END
-
-line_square ()
