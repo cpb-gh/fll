d297c25580779a8024cd0287f9adce3fd586169b
proritobot <proritos@googlegroups.com>
2022-11-12 00:29:17 +0000

    new functions merged

--- combined_functions.py
+++ combined_functions.py
@@ -612,7 +612,7 @@ def get_speed(start, end, percent):
 
 
 
-def turn_function(degrees=90, easing=None, stoptype='brake',startspeed=40, endspeed=30, motorletterleft='A', motorletterright='B',turntype='both'):
+def turn_function(degrees=90, easing=None, stoptype='brake',startspeed=40, endspeed=30, motorletterleft='A', motorletterright='B',turntype='both' ,also_end_if=None):
 
     neg = degrees<0
 
@@ -628,7 +628,8 @@ def turn_function(degrees=90, easing=None, stoptype='brake',startspeed=40, endsp
             keep_spinning = False
         elif not neg and degrees_now >= degrees :
             keep_spinning = False
-
+        if also_end_if is not None and also_end_if():
+            keep_spinning=False
         if keep_spinning:
             pct_degrees = degrees_now/degrees
             pct_power = pct_degrees
@@ -727,7 +728,7 @@ def get_motor_by_letter(port):
 # (start_run.py) def start_run( color_sensor_letter = 'C', delay = 1):
 # (test_function.py) def test_function():
 # (turn_code.py) def get_speed(start, end, percent):
-# (turn_code.py) def turn_function(degrees=90, easing=None, stoptype='brake',startspeed=40, endspeed=30, motorletterleft='A', motorletterright='B',turntype='both'):
+# (turn_code.py) def turn_function(degrees=90, easing=None, stoptype='brake',startspeed=40, endspeed=30, motorletterleft='A', motorletterright='B',turntype='both' ,also_end_if=None):
 # (utillity_functions.py) def get_motor_by_letter(port):
 
 
