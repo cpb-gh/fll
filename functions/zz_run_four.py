e34fefdb7eb7ef85e38a12141228443f573fedf9
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-11-23 14:04:13 -0500

    Create zz_run_four.py

---
+++ functions/zz_run_four.py
@@ -0,0 +1,7 @@
+
+### FUNCTION START
+def zz_run_four():
+    gyro_straight(start_power = 50, end_power = 50, degrees = 1350)
+    gyro_straight(start_power = 50, end_power = 50, degrees = -1350)
+    ### Blue. gets one energy and our model to the middle circle of the board
+### FUNCTION END
