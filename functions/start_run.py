ab57a7305d955b5196298daf719aa0a6910e251d
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-11-23 13:52:13 -0500

    Update start_run.py

--- functions/start_run.py
+++ functions/start_run.py
@@ -2,8 +2,6 @@ from spike import PrimeHub, LightMatrix, Button, StatusLight, ColorSensor, Speak
 from spike.control import wait_for_seconds
 
 ### FUNCTION START
-
-
 def start_run( color_sensor_letter = 'C', delay = 1):
     # color_sensor_letter = 'C' means that the active sensor is on the right side of the robot when it is facing the same direction as you
     color = ColorSensor(color_sensor_letter)
@@ -18,16 +16,26 @@ def start_run( color_sensor_letter = 'C', delay = 1):
             speaker.beep(60, delay)
             zz_run_one()
             waiting_for_run = False
-        elif the_color == 'yellow':
+        elif the_color == 'banana':
             print ( 'Detected:', the_color)
-            status_light.on('red')
+            status_light.on('yellow')
             speaker.beep(60, delay)
-            #zz_run_two()
+            zz_run_three()
+            waiting_for_run = False
+        elif the_color == 'green':
+            print ( 'Detected:', the_color)
+            status_light.on('green')
+            speaker.beep(60, delay)
+            zz_run_two()
+            waiting_for_run = False
+        elif the_color == 'blue':
+            print ( 'Detected:', the_color)
+            status_light.on('blue')
+            speaker.beep(60, delay)
+            zz_run_four()
             waiting_for_run = False
         else:
             print ('not a run color:', the_color)
             status_light.off()
-
-
 ### FUNCTION END
 start_run(color_sensor_letter = 'D', delay = 5)
