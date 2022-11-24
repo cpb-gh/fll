5b381ca43170d1860c9a6036a0dfdd604b22bf44
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-11-24 12:08:15 -0500

    Timeout seconds (#47)
    * Update turn_code.py
    
    * Update control_attachments.py
    
    * Update control_attachments.py
    
    * Update turn_code.py

--- functions/turn_code.py
+++ functions/turn_code.py
@@ -4,7 +4,7 @@ def get_speed(start, end, percent):
 
 
 
-def turn_function(degrees=90, easing=None, stoptype='brake',startspeed=40, endspeed=30, motorletterleft='A', motorletterright='B',turntype='both' ,also_end_if=None):
+def turn_function(degrees=90, easing=None, stoptype='brake',startspeed=40, endspeed=30, motorletterleft='A',also_end_if = None, motorletterright='B',turntype='both', timeout_seconds = 0):
 
     neg = degrees<0
 
@@ -13,6 +13,8 @@ def turn_function(degrees=90, easing=None, stoptype='brake',startspeed=40, endsp
 
     hub.motion_sensor.reset_yaw_angle()
     motors = MotorPair(motorletterleft, motorletterright)
+    t = Timer()
+    t.reset()
     keep_spinning = True
     while keep_spinning == True:
         degrees_now= hub.motion_sensor.get_yaw_angle()
@@ -22,11 +24,16 @@ def turn_function(degrees=90, easing=None, stoptype='brake',startspeed=40, endsp
             keep_spinning = False
         if also_end_if is not None and also_end_if():
             keep_spinning=False
+
+        if timeout_seconds != 0 and t.now() > timeout_seconds:
+            keep_spinning = False
+
         if keep_spinning:
             pct_degrees = degrees_now/degrees
             pct_power = pct_degrees
-
             
+            
+
             if easing is not None:
                 pct_power = easing(pct_degrees)
             speed = get_speed (startspeed, endspeed, pct_power)
