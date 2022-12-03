60e6ebcfc69ac5bb4a1f58c8062775dcd3e2ce75
sithforri <104786356+sithforri@users.noreply.github.com>
2022-12-03 17:48:27 -0500

    Update and rename zz_run_three to zz_run_three.py

--- functions/zz_run_three.py
+++ functions/zz_run_three.py
@@ -1,5 +1,5 @@
 ### START FUNCTION
-def zz_run_three():
+def power_plant_high_5():
     sensorL = ColorSensor ( 'D' )
     sensorR = ColorSensor ( 'C' )
     def see_black_either():
@@ -12,26 +12,25 @@ def zz_run_three():
         return sensorR.get_color() is 'white'
 
     # from start position, move to middle of field
-    # gyro_straight(degrees=1500, start_power=90, end_power=60, easing=ExponentialEaseInOut)
-    # wait_for_seconds(1)
+    gyro_straight(degrees=1500, start_power=90, end_power=60, easing=ExponentialEaseInOut, kp=4)
+    wait_for_seconds(1)
 
-    # #turn to get distance from the power plant
-    # turn_function(degrees=10, startspeed=40, endspeed=30)
-    # wait_for_seconds(1)
+    #turn to get distance from the power plant
+    turn_function(degrees=10, startspeed=40, endspeed=30)
+    wait_for_seconds(1)
 
-    # #move forward till black
-    # gyro_straight(degrees=360, start_power=30, end_power=30, easing=ExponentialEaseInOut, also_stop_if=see_black_either)
-    # wait_for_seconds(1)
-    # #keep on going
-    # gyro_straight(degrees= 80, start_power=40, end_power=40)
-    # wait_for_seconds(1)
-    # #now, swing your tail to unlock power plant energies
-    # turn_function(degrees=90, startspeed=40, endspeed=40, timeout_seconds=1, easing=ExponentialEaseOut)
-    # #and back up to let the energies out
-    # wait_for_seconds(1)
-    # gyro_straight(degrees=-360, start_power=40, end_power=40, kp=2)
+    #move forward till black
+    gyro_straight(degrees=360, start_power=30, end_power=30, easing=ExponentialEaseInOut, also_stop_if=see_black_either)
+    wait_for_seconds(1)
+    #keep on going
+    gyro_straight(degrees= 90, start_power=40, end_power=40)
+    wait_for_seconds(1)
+    #now, swing your tail to unlock power plant energies
+    turn_function(degrees=90, startspeed=40, endspeed=40, timeout_seconds=1, easing=ExponentialEaseOut)
+    #and back up to let the energies out
+    wait_for_seconds(1)
+    gyro_straight(degrees=-360, start_power=40, end_power=40, kp=4)
 
-    # topIf=see_black_right)
     for i in range(2):
         gyro_straight(degrees=-180, start_power=40, end_power=40, kp=2)
         gyro_straight(degrees=190, start_power=40, end_power=40,kp=2)
