82a37009bbd1de5e8a6e845b940240e0a78357d6
cpb-gh <104538915+cpb-gh@users.noreply.github.com>
2022-12-04 13:50:10 -0500

    Update zz_run_five.py

--- functions/zz_run_five.py
+++ functions/zz_run_five.py
@@ -1,4 +1,4 @@
-###  START FUNCTION
+### FUNCTION START
 ### this will put the mission model and energy into the middle and go to the right side of the board. This is the last run
 def zz_run_five():
     gyro_straight( left_motor_letter='A', right_motor_letter='B', degrees=1640, start_power=100, end_power=40, easing = None, motor_stop_mode = brake, kp = 0.5, timeout_seconds = 0 )
@@ -8,4 +8,4 @@ def zz_run_five():
     gyro_straight( left_motor_letter='A', right_motor_letter='B', degrees=720, start_power=50, end_power=50, easing = None, motor_stop_mode = brake, kp = 0.5, timeout_seconds = 0 )
     turn_function(degrees=-45, easing=None, stoptype='brake',startspeed=20, endspeed=20, motorletterleft='A',also_end_if = None, motorletterright='B',turntype='both', timeout_seconds = 0)
     gyro_straight( left_motor_letter='A', right_motor_letter='B', degrees=1100, start_power=50, end_power=50, easing = None, motor_stop_mode = brake, kp = 0.5, timeout_seconds = 0 )
-### END FUNCTION
+### FUNCTION END
