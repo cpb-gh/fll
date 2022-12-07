47675700d09f7293a78b0ef514254d3a012e54a1
Erik Blankinship <erikb@mediamods.com>
2022-12-07 16:06:34 -0500

    dump in the solar bucket & snag solar energies (#65)
    looks good

--- functions/zz_run_four.py
+++ functions/zz_run_four.py
@@ -1,20 +1,20 @@
 
 ### FUNCTION START
-### this will TRY to get to the power plant and fail.
 def zz_run_four():
-    gyro_straight(degrees = 360, start_power = 70, end_power = 60, easing=ExponentialEaseInOut, motor_stop_mode = brake,kp = 1)
-    turn_function(degrees=25, easing=None, stoptype='brake',startspeed=40, endspeed=40, motorletterleft='A',also_end_if = None, motorletterright='B',turntype='both', timeout_seconds = 0)
-    gyro_straight(degrees = 170, start_power = 70, end_power = 60, easing=ExponentialEaseInOut, motor_stop_mode = brake,kp = 1)
-    turn_function(degrees=-25, easing=None, stoptype='brake',startspeed=40, endspeed=40, motorletterleft='A',also_end_if = None, motorletterright='B',turntype='both', timeout_seconds = 0)
-    def sensed_black_right(letter_one = 'C', letter_two = 'D'):
-        color_sensor_one = ColorSensor(letter_one)
-        color_sensor_two = ColorSensor(letter_two)
-        color_one = color_sensor_one.get_color()
-        color_two = color_sensor_two.get_color()
-        if color_one == 'black':
-            return True
-        else:
-            return False
-    gyro_straight(degrees = 720, start_power = 50, end_power = 40, easing=ExponentialEaseInOut, motor_stop_mode = brake,kp = 1, also_stop_if = sensed_black_right)
-    line_follow( Sspeed=30, Espeed=30, sensorLetter="C", stopIf=None, stopMode='brake', degrees=900, motorLeftletter = 'A', motorRightletter='B')
+    sensorL = ColorSensor ( 'D' )
+    sensorR = ColorSensor ( 'C' )
+    def see_black_left():
+        return sensorL.get_color() is 'black'
+    def see_black_right():
+        return sensorR.get_color() is 'black'
+
+    gyro_straight(degrees=1000, start_power=50, end_power=50, also_stop_if=see_black_left, kp=1)
+    line_follow(degrees=750, stopIf=see_black_right)
+    turn_function(degrees=-5, turntype='left', timeout_seconds=1, startspeed=40, endspeed=40)
+    gyro_straight(degrees=400, start_power=100, end_power=100, timeout_seconds=3)
+    control_attachments(degrees_wanted=-100, start_speed=100, end_speed=100, motor_letter='E', timeout_seconds=1)
+    control_attachments(degrees_wanted=-150 ,start_speed=70, end_speed=70, motor_letter='F', timeout_seconds=1)
+    gyro_straight(degrees=-400, start_power=40, end_power=40)
+    turn_function(degrees=25, turntype='left', startspeed=40, endspeed=40)
+    gyro_straight(degrees=-2000, start_power=100, end_power=100, kp=2)
 ### FUNCTION END
