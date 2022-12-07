159caef16e91766d2b04adcc157ac35aee7f762e
proritobot <proritos@googlegroups.com>
2022-12-07 12:27:21 +0000

    new functions merged

--- combined_functions.py
+++ combined_functions.py
@@ -819,20 +819,21 @@ def zz_run_three():
     wait_for_seconds(1)
     #keep on going
     gyro_straight(degrees= 110, start_power=40, end_power=40)
+    control_attachments(motor_letter="E",start_speed=40, end_speed=40, degrees_wanted=-1400)
     wait_for_seconds(1)
     #now, swing your tail to unlock power plant energies
     turn_function(degrees=90, startspeed=40, endspeed=40, timeout_seconds=1, easing=ExponentialEaseOut, turntype="both")
     #and back up to let the energies out
     wait_for_seconds(1)
-    control_attachments(motor_letter="E",start_speed=40, end_speed=40, degrees_wanted=-500)
-    gyro_straight(degrees=-400, start_power=80, end_power=60, kp=1)
+    gyro_straight(degrees=-450, start_power=80, end_power=60, kp=1)
     #forward to the high five!
     gyro_straight(degrees=500, start_power=40, end_power=40, kp=1)
     #turn_function(degrees=45,startspeed=40,endspeed=40,also_end_if=see_black_left)
     wait_for_seconds(1)
-    gyro_straight(degrees=400)
-    control_attachments(motor_letter="E", start_speed=40, end_speed=40, degrees_wanted=-1500)
-    control_attachments(motor_letter="E", start_speed=40, end_speed=40, degrees_wanted=1500)
+    gyro_straight(degrees=380)
+    control_attachments(motor_letter="E", start_speed=40, end_speed=40, degrees_wanted=-1000)
+    control_attachments(motor_letter="E", start_speed=40, end_speed=40, degrees_wanted=1600)
+    gyro_straight(degrees=-200, start_power=30, end_power=30, kp=1)
     turn_function(degrees=-40, startspeed=40, endspeed=40, timeout_seconds=1, easing=ExponentialEaseOut, turntype="both")
 
     # for i in range(2):
