772b69268662a802b09daf5cd6ef8503a7735ecc
proritobot <proritos@googlegroups.com>
2023-09-04 15:25:21 +0000

    new functions merged

--- combined_functions.py
+++ combined_functions.py
@@ -403,35 +403,41 @@ def line_follow( Sspeed=40, Espeed=20, sensorLetter="D", stopIf=None, stopMode='
     if stopMode is not None:
         motor_pair.set_stop_action(stopMode)
     while stop == False:
+        # if we've gone as far as we need to go, then stop
         mdeg1 = motor1.get_degrees_counted()
         mdeg2 = motor2.get_degrees_counted()
         motordeg = (abs(mdeg1) + abs(mdeg2))/ 2
         if motordeg >= degrees:
             stop = True
 
+        # this is how close to the final distance (degrees) we are,
+        # then we set "speed" to the speed we want to be at
+        # based on how close to the end we are.
         pct = motordeg/degrees
+        speed = get_speed( Sspeed, Espeed, pct )
 
-        speedy = get_speed( Sspeed, Espeed, pct )
-        # print('speedy', speedy )
+        # now we calculate P_fix, I_fix, and D_fix for our PID function
+        # Proportional, Integral, Derivative
 
-
-
-        speed = speedy
-
+        # error should be negative if we need to turn one direction or positive the other direction
+        # get_reflected_light is a number 0 (black) - 100 (white) so subtracting 50 gets us -50 - 50
         error = color.get_reflected_light() - 50
-        P_fix = error * kp
+        
+        P_fix = error * kp # P_fix is the error above times a factor - it's a snapshot/point in time correction
 
+        # integral starts at 0 and then we add the error (which can be negative), so this is accumulating error over time
         integral = integral + error
+        I_fix = integral * 0.001 # 0.001 is like the kp for I_fix
 
-        I_fix = integral * 0.001
-
+        # lastError starts at 0 and gets subtracted from the current error to get "derivative"
+        # so this is a trailing indicator of the last two error amounts
         derivative = error - lastError
         lastError = error
-        D_fix = derivative * 1
+        D_fix = derivative * 1 # the kp for D_fix is 1
 
-        correction = P_fix+I_fix+D_fix
+        correction = P_fix+I_fix+D_fix # correct is the sum of our P, I, and D_fix's
 
-        motor_pair.start_tank_at_power(int(speed+correction), int(speed-correction))
+        motor_pair.start_tank_at_power(int(speed+correction), int(speed-correction)) # add the correction from one wheel and subtract from the other to cause a turn
 
         if stop == True and stopMode is not None:
             motor_pair.stop()
